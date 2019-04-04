bcb5230886211beda99c22a7438032cf9431a1f9
bommojuvishu <34394466+bommojuvishu@users.noreply.github.com>
2019-04-04 21:50:51 +0530

    new approach

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT | 3000;
+const http = require("http");
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello World</h1>");
+});
+server.listen(port, () => {
+  console.log(`Server running at port ` + port);
+});
