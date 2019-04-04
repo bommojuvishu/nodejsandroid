b01da9a5eb47a4fde130dd33b3f19924f7cb867c
bommojuvishu <34394466+bommojuvishu@users.noreply.github.com>
2019-04-04 22:19:01 +0530

    msg

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const http = require("http");
-
 const port = process.env.PORT || 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World</h1>");
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello world!");
 });
-server.listen(port, () => {
-  console.log(`Server running at port ` + port);
-});
+
+app.listen(port);
