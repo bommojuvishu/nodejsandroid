55efecb2be72a05e8d480c683d70ae07ea616a67
bommojuvishu <34394466+bommojuvishu@users.noreply.github.com>
2019-04-13 10:06:59 +0530

    conflict 2

--- index.js
+++ index.js
@@ -49,7 +49,6 @@ app.post("/upload", function(req, res) {
 });
 
 app.get("/", function(req, res) {
-<<<<<<< HEAD
   request(url)
     .pipe(writeFile)
     .on("close", function() {
@@ -66,9 +65,6 @@ app.get("/", function(req, res) {
           process.exit(0);
         });
     });
-=======
-  res.send("god says you have to go the shazam movie!");
->>>>>>> 33bc8776241d1d34dc1c87cca195a54507a4b5ab
 });
 
 app.listen(port, () => {
