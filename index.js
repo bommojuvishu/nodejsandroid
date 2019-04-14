caedf7da650cef5ce2c218e4867dea37098cd124
bommojuvishu <34394466+bommojuvishu@users.noreply.github.com>
2019-04-14 15:50:34 +0530

    enable cors

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const app = express();
 
+// enable cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // default options
 app.use(fileUpload());
 
